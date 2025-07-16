d8fbb25548818d1b92d81d47aa3405b61371943f
Salman Malik <salman.the.malik@gmail.com>
2025-07-16 12:56:29 -0400

    first commit

---
+++ svc-aaa/src/main/java/com/example/bbb/AaaApplication.java
@@ -0,0 +1,12 @@
+package com.example.bbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaApplication.class, args);
+	}
+}
---
+++ svc-aaa/src/main/java/com/example/bbb/MyController.java
@@ -0,0 +1,29 @@
+package com.example.bbb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    private final String myVal;
+    private final int instanceId;
+
+    public MyController(@Value("${my.val}") String myVal, @Value("${instance.id:1}")  int instanceId) {
+        this.myVal = myVal;
+        this.instanceId = instanceId;
+    }
+
+    @GetMapping("/req")
+    public ReturnVal getMyVal(@RequestHeader("my-header") String headerVal) {
+        return new ReturnVal(myVal, headerVal, instanceId);
+    }
+
+	record ReturnVal(
+		String val,
+		String header,
+        int instanceId
+	) { }
+}
