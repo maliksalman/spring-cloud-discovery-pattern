d3e9ca7134ec918a774f33b1321c898ef661acc4
Salman Malik <salman.the.malik@gmail.com>
2025-07-24 15:13:15 -0400

    adding /lb/{svc-name} endpoint to demonstrate client-side load-balancing

--- svc/src/main/java/com/example/svc/SvcController.java
+++ svc/src/main/java/com/example/svc/SvcController.java
@@ -2,18 +2,24 @@ package com.example.svc;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 public class SvcController {
 
     private final String myVal;
     private final int instanceId;
+    private final RestClient loadBalancedClient;
 
-    public SvcController(@Value("${my.val}") String myVal, @Value("${instance.id:1}")  int instanceId) {
+    public SvcController(@Value("${my.val}") String myVal,
+                         @Value("${instance.id:1}")  int instanceId,
+                         RestClient.Builder loadBalancedClientBuilder) {
         this.myVal = myVal;
         this.instanceId = instanceId;
+        this.loadBalancedClient = loadBalancedClientBuilder.build();
     }
 
     @GetMapping("/req")
@@ -21,9 +27,12 @@ public class SvcController {
         return new ReturnVal(myVal, headerVal, instanceId);
     }
 
-	record ReturnVal(
-		String val,
-		String header,
-        int instanceId
-	) { }
+    @GetMapping("/lb/{service}")
+    public ReturnVal getLoadBalancedClientVal(@PathVariable("service") String serviceName) {
+        return loadBalancedClient.get()
+                .uri("lb://" + serviceName + "/req")
+                .header("my-header", "discovery-client")
+                .retrieve()
+                .body(ReturnVal.class);
+    }
 }
